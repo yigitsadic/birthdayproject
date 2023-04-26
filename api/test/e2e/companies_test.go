878cfa032e3c3157e4d1396f06982be8beb6e09e
Yigit Sadic <yigitsadic@gmail.com>
2023-04-26 22:46:08 +0300

    added company e2e tests

--- api/test/e2e/companies_test.go
+++ api/test/e2e/companies_test.go
@@ -80,11 +80,37 @@ func (testSuite *IntegrationTestSuite) Test_Companies_Update() {
 		assert.Equal(http.StatusOK, resp.StatusCode)
 	})
 
-	testSuite.Run("it should update company data", func() {
-		assert.True(true)
+	testSuite.Run("it should handle bad input", func() {
+		badInput := `{"name": ""}`
+		req, err := http.NewRequest(
+			http.MethodPut,
+			genEndpoint(recordId),
+			bytes.NewBufferString(badInput),
+		)
+		req.Header.Add("Authorization", "Bearer "+token)
+
+		assert.Nil(err)
+
+		resp, err := http.DefaultClient.Do(req)
+
+		assert.Nil(err)
+		assert.Equal(http.StatusUnprocessableEntity, resp.StatusCode)
 	})
 
-	testSuite.Run("it should update company data", func() {
-		assert.True(true)
+	testSuite.Run("it should handle if authenticated tries to access another resource", func() {
+		goodBody := `{"name": "very nice company"}`
+		req, err := http.NewRequest(
+			http.MethodPut,
+			genEndpoint(9999999),
+			bytes.NewBufferString(goodBody),
+		)
+		req.Header.Add("Authorization", "Bearer "+token)
+
+		assert.Nil(err)
+
+		resp, err := http.DefaultClient.Do(req)
+
+		assert.Nil(err)
+		assert.Equal(http.StatusNotFound, resp.StatusCode)
 	})
 }
