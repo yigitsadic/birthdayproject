2946d17b660117afca32299660d17b3b892f8ab1
Yigit Sadic <yigitsadic@gmail.com>
2023-04-30 21:13:42 +0300

    created static-site project with next

---
+++ static-site/app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Birthday App",
+  description: "Send birthday emails to your employees!",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
