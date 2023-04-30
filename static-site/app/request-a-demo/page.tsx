83e7ea8aac4738bc0e6b5faf588240c01ed98c87
Yigit Sadic <yigitsadic@gmail.com>
2023-05-01 01:02:50 +0300

    added request a demo content from chatgpt

--- static-site/app/request-a-demo/page.tsx
+++ static-site/app/request-a-demo/page.tsx
@@ -1,7 +1,41 @@
 export default function RequestADemoPage() {
   return (
     <div>
-      <h2>This is request a demo page</h2>
+      <h2>Request a demo</h2>
+
+      <p className="lead">
+        Thank you for your interest in Cakeday.today! To request a demo of our
+        service, simply fill out the registration form below.
+      </p>
+
+      <p>
+        Once you submit your registration, our team will review it and evaluate
+        whether Cakeday.today is the right fit for your company. During the
+        review process, you'll be able to use our system and explore its
+        features. However, please note that your employees won't receive any
+        birthday emails until your registration has been approved.
+      </p>
+      <p>
+        During the review process, we'll be looking at a number of factors to
+        ensure that our service meets your needs. We'll evaluate your company's
+        size, budget, and goals, as well as your existing HR and employee
+        engagement strategies.
+      </p>
+
+      <p>
+        Once your registration has been approved, you'll be able to start using
+        our service to send personalized birthday emails to your employees. Our
+        system is easy to use, with customizable templates and automated
+        scheduling to save you time and effort.
+      </p>
+
+      <p>
+        At Cakeday.today, we're committed to helping companies create a more
+        positive and engaging workplace culture. We believe that recognizing and
+        celebrating your employees' birthdays is an important part of that
+        process. Request a demo today and see how our service can help you show
+        your employees how much you appreciate them!
+      </p>
     </div>
   );
 }
