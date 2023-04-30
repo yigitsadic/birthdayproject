23e8b66211fc724f31b60cfd94e3f5f3052c57f7
Yigit Sadic <yigitsadic@gmail.com>
2023-04-30 20:21:16 +0300

    updated queries, fixed error

--- api/internal/postgres_store/pg_employees/queries.go
+++ api/internal/postgres_store/pg_employees/queries.go
@@ -5,7 +5,7 @@ const (
 select id, company_id, first_name, last_name, email, birth_day, birth_month
 from employees
 where employees.company_id = $1
-order by first_name, last_name
+order by employees.id DESC
 `
 
 	findOneQuery = `
@@ -15,13 +15,13 @@ where employees.company_id = $1 and employees.id = $2
 	`
 
 	createQuery = `
-insert into employees(company_id, first_name, last_name, email, birth_day, birth_month)
-values ($1, $2, $3, $4, $5, $6)
+insert into employees(company_id, first_name, last_name, email, birth_day, birth_month, created_at, updated_at)
+values ($1, $2, $3, $4, $5, $6, now(), now())
 returning id, company_id, first_name, last_name, email, birth_day, birth_month
 	`
 
 	updateQuery = `
-update employees set first_name = $1, last_name = $2, email = $3, birth_day = $4, birth_month = $5
+update employees set first_name = $1, last_name = $2, email = $3, birth_day = $4, birth_month = $5, updated_at=now()
 where employees.company_id = $6 and employees.id = $7
 returning id, company_id, first_name, last_name, email, birth_day, birth_month
 `
