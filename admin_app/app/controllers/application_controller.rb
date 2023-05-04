f96a2fedffe579d65f51030fe954e724678df181
Yigit Sadic <yigitsadic@gmail.com>
2023-05-04 23:09:25 +0300

    added basic auth

--- admin_app/app/controllers/application_controller.rb
+++ admin_app/app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with name: ENV['ADMIN_USER_NAME'], password: ENV['ADMIN_PASSWORD']
+
   def set_companies
     @companies = Company.all.order(name: :asc)
   end
